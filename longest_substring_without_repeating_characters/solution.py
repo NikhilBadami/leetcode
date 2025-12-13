195100e8fdfd66ef38c12c8bb5aff6a482175a07
NikhilBadami <nikhilbadami@Nikhils-MacBook-Pro.local>
2025-12-13 15:37:29 -0500

    Longest substring without repeating characters intuitive solution

--- longest_substring_without_repeating_characters/solution.py
+++ longest_substring_without_repeating_characters/solution.py
@@ -1,32 +1,30 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         """
-        Idea is to find the longest substring without repeating characters, or, put another way, to find the longest
-        substring with unique characters. I can use a sliding window to search the array, combined with a set to track
-        unique characters. As I expand the window, I add characters to the set. If I find a character in the set, I begin
-        shrinking the window and removing characters as I go, until all the characters in the window are unique once again.
+        Goal is to find the longest substring without duplicat characters. In other words, the substring should only include
+        unique characters. I can solve this problem using a set and a sliding window. The set keeps track of the characters
+        in the window. When I encounter a duplicate character, I shrink the window and remove characters from the set
+        until there are no more duplicate characters.
 
         time: O(n)
         memory: O(n)
         """
-        # Edge case if s is empty
+        max_len = 0
         if len(s) == 0:
-            return 0
-
-        l, r = 0, 0
+            return max_len
+        
         unique_chars = set()
-        max_len = -1
-
+        l, r = 0, 0
         while r < len(s):
             if s[r] not in unique_chars:
                 unique_chars.add(s[r])
                 r += 1
             else:
                 max_len = max(max_len, r - l)
-                while l <= r and s[r] in unique_chars:
+                while s[r] in unique_chars and l < r:
                     unique_chars.remove(s[l])
                     l += 1
-        # Final check
+        # Handle case where we get to the end of the string without encountering duplicates
         max_len = max(max_len, r - l)
         return max_len
         
