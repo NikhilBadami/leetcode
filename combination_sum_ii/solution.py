8409783d204b29ba77a0cc9400e3c1f56928003c
NikhilBadami <nikhilbadami@Nikhils-MacBook-Pro.local>
2025-12-28 14:15:09 -0500

    Combination Sum-II optimal solution

--- combination_sum_ii/solution.py
+++ combination_sum_ii/solution.py
@@ -1,53 +1,57 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         """
-        This problem is similar to the subsets problem where I cannot use numbers more than once. This means the branching
-        factor is the same as the subsets problem: either I use the current element or I don't and then proceed to the
-        next index. The caveat with this problem is I only return solutions that sum to target. This means that instead
-        of the queue just storing the existing subsets, they must store tuples that contain both the existing subset so
-        far as well as the running sum of that subset. Additionally, because I have a target who's solution I am looking for
-        I can get a small efficiency increase by pre-sorting the array so that I can stop early if I encounter any value
-        greater than the target, preventing unnecessary tree exploration
+        This problem is similar to the original combination sum problem, except instead of being able to re-use a number
+        an infinite number of times, we are limited to the number of times the number appears in the candidates array.
+        While iterating through, I can define my recursion tree as follows: The left sub-tree is allowed to re-use the
+        current element. The right subtree cannot and must move on to the next unique value in candidates. If the left
+        sub-tree exhausts the current element, it is allowed to use the next unique element it encounters. Note that to
+        achieve this I will need to sort the input before processing, however, compared to the overall runtime, this is
+        not a bottle neck for time efficiency.
 
-        time: O(2^n) --> Technically there is an O(nlog(n)) pre-processing time but this is dwarfed by the overall run
-        time complexity of the algorithm
-        memory: O(2^n) worst case
+        time: O(n * 2^n) --> Each element can only be used once and we loop during recursion to skip elements
+        memory: O(2^n)
         """
-        # Pre-sort array
+        # Sort input to make it easier to detect duplicates
         candidates.sort()
-
-        from collections import deque
-        q = deque()
-        
-        i = 0
-        q.append(([], 0))
-        while i < len(candidates):
-            if candidates[i] > target:
-                # No possible solutions exist beyond this point
-                break
-
-            cur_sets = len(q)
-            for _ in range(cur_sets):
-                # Pop off existing solutions from the queue. For each one, create a copy and then append the current
-                # index to the copy while keeping the original
-                s = q.popleft()
-                cur_soln, cur_sum = s
-                new_soln = cur_soln.copy()
-                new_soln.append(candidates[i])
-                new_sum = cur_sum + candidates[i]
-                q.append((cur_soln, cur_sum))
-                # Only add the new solution if its sum <= target
-                if new_sum <= target:
-                    q.append((new_soln, new_sum))
-
-            # Each number can only be used once, so if the next value equals the current value, it should be skipped
-            i += 1
-    
-        # post-process q to get solutions that equal target
         res = []
-        while len(q):
-            soln, _sum = q.popleft()
-            if _sum == target:
-                res.append(tuple(soln))
-        return list(set(res))
+        self._helper(candidates, target, res, 0, ([], 0))
+        return res
+    
+    def _helper(
+        self,
+        candidates,
+        target,
+        res,
+        cur_idx,
+        cur_soln
+    ):
+        """
+        Helper function used to calculate solution. Takes the input from the main function, a list to track the current
+        solution, the current index and a list tracking the overall solution
+        """
+        # Handle base cases
+        cur_list, cur_sum = cur_soln
+        if cur_sum == target:
+            res.append(cur_list.copy())
+            return
+        if cur_idx >= len(candidates) or cur_sum > target:
+            return
+        
+        # Create left sub-tree
+        cur_list.append(candidates[cur_idx])
+        cur_sum += candidates[cur_idx]
+        self._helper(candidates, target, res, cur_idx+1, (cur_list, cur_sum))
+
+        # Undo left operations and create right sub-tree
+        cur_list.pop()
+        cur_sum -= candidates[cur_idx]
+        next_idx = cur_idx + 1
+        while next_idx < len(candidates) and candidates[cur_idx] == candidates[next_idx]:
+            next_idx += 1
+        self._helper(candidates, target, res, next_idx, (cur_list, cur_sum))
+        
+        return
+
+
         
