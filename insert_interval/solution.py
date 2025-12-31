4f9da5b6c8093f88f831d1577800797cf4a7b12a
NikhilBadami <nikhilbadami@Nikhils-MacBook-Pro.local>
2025-12-31 11:33:16 -0500

    Insert interval intuitive solution

--- insert_interval/solution.py
+++ insert_interval/solution.py
@@ -1,47 +1,43 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         """
-        The goal of this problem is to insert a new interval into a list of intervals sorted by their start time. After
-        insertion there should be no overlapping intervals, so interval should be merged if necessary.
+        I'm given a set of intervals that do not overlap and are sorted in ascending order by start time. I need to insert a new
+        interval into this list such that the non-overlapping and sorted nature is maintained. I can merge intervals if necessary
+        to help maintain this property. There are many cases where a new interval could overlap with an existing one, for example,
+        the new interval could start in an existing interval but end after, it could end in an existing interval and start before,
+        it could be completely subsumed by an existing interval or vice versa. Trying to explicitly code all of these conditions
+        could get hairy, so instead, it may be better to consider the conditions where the new interval does not overlap with an
+        existing interval. While iterating through the intervals list, for any given interval I can check the following conditions:
 
-        Merging intervals will fall under one of the three conditions below:
+        1: If the end time of the new interval occurrs before the start of the current interval, insert the new interval first
+           followed by the existing interval. Because the input array is sorted, the first interval that satisfies this condition
+           is guaranteed to be mark the insertion point
+        2: If the end of the existing interval comes before the start of the new interval, insert the existing interval
+        3: If the above conditions are not met, the intervals overlap and should be merged
 
-        1: If the end of the new interval comes before the start of an existing interval, insert the new interval first,
-           then insert the existing interval. Assume the interval did not overlap with any previous intervals
-        2: If the start of the new interval comes after the end of an existing one, insert the existing interval, but
-           do not insert the new interval yet as it may overlap with future intervals
-            - Have a check in case we make it to the end of iteration and the interval was not inserted
-        3: If neither of the above conditions are true, the new interval overlaps with the existing interval. In this
-           case create a new merge interval as follows:
-                new_start = min(insert_start, existing_start)
-                new_end = max(insert_end, existing_end)
-            continue iteration and try to either insert or merge this new interval based on the above 3 conditions
-        
         time: O(n)
-        memory: O(n)
+        memory: O(1)
         """
-        # Edge case
+        # Check edge case
         if len(intervals) == 0:
             return [newInterval]
         
         res = []
         inserted = False
-        for interval in intervals:
-            if newInterval[1] < interval[0]:
-                # Condition 1
+        for i in range(len(intervals)):
+            if newInterval[1] < intervals[i][0]:
                 if not inserted:
                     res.append(newInterval)
-                res.append(interval)
+                res.append(intervals[i])
                 inserted = True
-            elif newInterval[0] > interval[1]:
-                # Condition 2
-                res.append(interval)
+            elif intervals[i][1] < newInterval[0]:
+                res.append(intervals[i])
             else:
-                # Condition 3
-                new_start = min(newInterval[0], interval[0])
-                new_end = max(newInterval[1], interval[1])
-                newInterval = [new_start, new_end]
+                # Intervals overlap and should be merged. Do not merge as the new merged interval could overlap with other intervals
+                merge_start = min(newInterval[0], intervals[i][0])
+                merge_end = max(newInterval[1], intervals[i][1])
+                newInterval = [merge_start, merge_end]
         if not inserted:
             res.append(newInterval)
         return res
-
+        
