8be28aa470827f6a6ac8fdb102d19d5423e9d4c9
NikhilBadami <nikhilbadami@Nikhils-MacBook-Pro.local>
2026-01-10 13:05:14 -0500

    walls and gates incorrect bfs approach

--- walls_and_gates/solution.py
+++ walls_and_gates/solution.py
@@ -4,60 +4,50 @@ class Solution:
         I'm given an mxn matrix with three values representing either water, land or a treasure
         chest. I need to find the distance of each land cell from its nearest treasure chest.
 
-        One way to do this would be to start a dfs from each land cell and find the closest
-        treasure chest. This would work but means I may recurse through land cells multiple
-        times if my search moves through them while searching for a treasure chest. These
-        additional land cells to not have their information updated during these recursive
-        calls which leads to wasted effort.
+        I can start a breadth first search from each treasure cell. The values put in the queue
+        will be a tuple of (cell_val, distance) and the distance will be updated only if it is
+        less than the current distance of the cell. I maintain a visited set so that individual
+        searches do not process a given cell multiple times. This ensures that cells are
+        processed "level by level" and that the same distance is passed to each cell on the same
+        level.
 
-        Another way to approach the problem would be to iterate from each *treasure chest*
-        Starting from each treasure chest, I run a dfs that includes a counter showing how far
-        this particular part of the search is from the starting chest. For each land cell I
-        encounter, I set its value to this counter. While iterating, I take the
-        min(cur_value, counter) for each land cell. This way, if I start a search from another
-        chest that happens to be closer to a given land cell, the cell is correctly updated.
-
-        time: O(t) --> Because I'm starting the search from a treasure chest, I only process each
-                       cell in the graph up to t times
-        memory: O(m*n) --> The size of the call stack which in the worst case is the size of the
-                           input grid
+        time: O(t) --> t is the number of chests in the grid
+        memory: O(m*n) --> worst case if every land cell is reachable from every other land cell
+                           all are stored in visited set
         """
+        from collections import deque
         for i in range(len(grid)):
             for j in range(len(grid[i])):
                 if grid[i][j] == 0:
-                    # Set of tuples that tracks pairs (i,j) of visited land cells
                     visited = set()
-                    # Search starting from this point
-                    self._helper(grid, i-1, j, 1, visited)
-                    self._helper(grid, i+1, j, 1, visited)
-                    self._helper(grid, i, j-1, 1, visited)
-                    self._helper(grid, i, j+1, 1, visited)
+                    self.bfs(grid, i, j, visited)
+        
         return
     
-    def _helper(self, grid, i, j, distance, visited):
+    def bfs(self, grid, i, j, visited):
         """
-        Helper function to perform dfs starting from a treasure chest. Updates land cells
-        with their distance from the chest that started the search. Updates grid in place
+        Helper function to run a bfs starting from the given cell. Modifies the grid in place
         """
-        if i >= len(grid) or i < 0:
-            return
-        if j >= len(grid[i]) or j < 0:
-            return
-        if (i, j) in visited:
-            return
-        if grid[i][j] == -1 or grid[i][j] == 0:
-            # Do not update water or chest cells
-            return
-        
-        # Update land cells based on distance from the starting chest
-        grid[i][j] = min(grid[i][j], distance)
-        # visited.add((i, j))
-        distance += 1
-        self._helper(grid, i-1, j, distance, visited)
-        self._helper(grid, i+1, j, distance, visited)
-        self._helper(grid, i, j-1, distance, visited)
-        self._helper(grid, i, j+1, distance, visited)
-
-        return
-
+        distance = 0
+        q = deque()
+        q.append((i, j, distance))
+        while len(q) != 0:
+            # Get size of "level" in queue
+            size = len(q)
+            for _ in range(size):
+                # Process current cell. Only update if it is land. Note that the search is
+                # initialized with the treasure cell
+                if grid[i][j] > 0:
+                    grid[i][j] = min(grid[i][j], distance)
+                    visited.add((i, j))
+                # Process each neighbor cell. Only add land cells to q
+                i, j, distance = q.popleft()
+                if i - 1 >= 0 and grid[i-1][j] > 0 and (i-1, j) not in visited:
+                    q.append((i-1, j, distance + 1))
+                if i + 1 < len(grid) and grid[i+1][j] > 0 and (i+1, j) not in visited:
+                    q.append((i+1, j, distance + 1))
+                if j - 1 >= 0 and grid[i][j-1] > 0 and (i, j-1) not in visited:
+                    q.append((i, j-1, distance + 1))
+                if j + 1 < len(grid[i]) and grid[i][j+1] > 0 and (i, j+1) not in visited:
+                    q.append((i, j+1, distance + 1))
         
