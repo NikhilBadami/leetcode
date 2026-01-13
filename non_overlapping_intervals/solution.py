62a151854dd1deb4b7c41f1346a73c52da0ab7ad
NikhilBadami <nikhilbadami@Nikhils-MacBook-Pro.local>
2026-01-13 08:18:00 -0500

    Non overlapping intervals optimal solution

--- non_overlapping_intervals/solution.py
+++ non_overlapping_intervals/solution.py
@@ -1,40 +1,31 @@
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
         """
-        I am given an array of intervals and need to determine the minimum number of intervals I can remove to make the intervals non-overlapping.
-        Determining if two intervals overlap is straight forward. For a given interval i, I can check if two intervals overlap by checking if the
-        end time of the current interval is less than or equal to the start time of the next interval. If so, they do not overlap. If not, they
-        do overlap (assume this check already passed for previous intervals starting at i=0).
+        I'm given an array of intervals and I need to determine the minimum number of intervals to remove such that all remaining intervals do not
+        overlap. I can do this as follows: first, I sort the array by start time. Then, I iterate through the array and make a series of
+        comparisons. I start with the first interval in the list and then compare it to subsequent intervals. For every interval the first
+        interval overlaps with, I keep the interval that ends earlier. I can determine if two intervals overlap by checking if the start time
+        of the subsequent interval comes after the end time of the current interval. If this condition is not true, the intervals overlap. If
+        the intervals do not overlap, I update the current interval to the one with the later end time. I only need a single counter to track
+        how many intervals I am removing. Every time two intervals overlap, I increment this counter.
 
-        Keep in mind, I do not need to remove any intervals or modify the input, I simply need to determine the number of intervals I would have
-        to remove. One thing I can do is split the intervals into two separate arrays, start and end. I can then sort each of these arrays and
-        iterate through them seperately. I can think of the intervals as being set down along a number line. I start at time == 1, and as I
-        iterate, I check to see how many intervals are started before the next timestamp in the end interval. The number of intervals to remove
-        is the max number of overlapping intervals at any given time - 1 (since I need to keep one of the intervals).
-
-        time: O(nlog(n)) --> Initial sort time
-        memory: O(n)
+        time: O(nlog(n)) --> sorting
+        memory: O(1)
         """
-        # Pre-process intervals and sort arrays
-        start = [i[0] for i in intervals]
-        end = [i[1] for i in intervals]
-        start.sort()
-        end.sort()
+        # Sort input by start time
+        intervals.sort(key=lambda x: x[0])
 
-        # Iterate through both arrays. Count the number of total overlapping arrays at any given time. This valus minus 1 is the number of
-        # intervals to remove
+        # Starting with the first interval in the list, iterate through and perform comparisons
         min_remove = 0
-        num_overlapping = 0
-        s = 0
-        e = 0
-        while s < len(start):
-            if start[s] < end[e]:
-                s += 1
-                num_overlapping += 1
-                min_remove = max(min_remove, num_overlapping - 1)
+        cur_interval = intervals[0]
+        for i in range(1, len(intervals)):
+            # Check if current interval overlaps with intervals[i]
+            if intervals[i][0] >= cur_interval[1]:
+                # Intervals do not overlap. Update current interval
+                cur_interval = intervals[i]
             else:
-                # Technically we process ends first since [1,2] and [2,3] are technically not overlapping
-                e += 1
-                num_overlapping -= 1
+                # Intervals overlap. Keep the one with the earlier end time
+                cur_interval = cur_interval if cur_interval[1] < intervals[i][1] else intervals[i]
+                min_remove += 1
         return min_remove
-
+        
