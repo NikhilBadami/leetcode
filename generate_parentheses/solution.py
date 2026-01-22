15bb477d896fccd7db41ab9950415b29c91e05a8
NikhilBadami <nikhilbadami@Nikhils-MacBook-Pro.local>
2026-01-21 20:01:29 -0500

    Generate parentheses optimal solution

--- generate_parentheses/solution.py
+++ generate_parentheses/solution.py
@@ -4,35 +4,50 @@ class Solution:
         I am given a number, n, which represents the number of pairs of parentheses I can work with. I need to generate all possible combinations
         of well-formed parentheses that can be generated.
 
-        I have to start with an open parenthesis. From there, I have two options: either I can add another open parenthesis if I still have the
-        budget to do so (i.e., n > 0) or I can close the current parenthesis. I can also use a tracker to keep track of how many unclosed
-        parenthesis I have. Once I have exhausted my budget of open parenthesis, I can reference this counter to determine how many closing
-        parenthesis I need to add.
+        I have to start with an open parenthesis. From here I can make two choices, either add another open parenthesis or add a closed one. I
+        can only add a closing parenthesis if there is a corresponding open one, otherwise I may generate an invalid solution.
 
-        time: O(2^n) --> For each parenthesis pair, I can choose to add an open parenthesis or close an existing one. I can do this for all n
-                         pairs.
-        memory: O(2^n) --> Need to generate all intermediate solutions
+        I can keep track of the number of open and close parentheses a current solution has. If the number of open parentheses exceeds the number
+        of closed parentheses, I can add closed parentheses. Otherwise, I cannot. Additionally, I can only add parenthesis of any kind as long
+        as the total number of each kind does not exceed n. 
+
+        time: O(4^n) --> Length of output is 2*number of pairs
+        memory: O(n) --> Recursive depth is at most n. Using arrays to build solution cuts down on memory
         """
-        return self._helper("(", n-1, 1)
+        return self._helper(n, [], 0, 0)
     
-    def _helper(self, cur_soln: str, n: int, num_open: int) -> List[str]:
+    def _helper(self, n, cur_soln, num_open, num_closed):
         """
-        Helper function that returns a list of solutions found. Takes n, the number of available pairs, and num_open, a counter which counts how
-        many un-paired open parenthesis there are in the current solution.
+        Helper function that helps build solutions. Takes n, the current solution and the number of open and closed parentheses present in the
+        current solution. Returns list of strings
         """
-        soln = []
-        if n == 0:
-            # Out of parenthesis pairs
-            # Check if there are un-closed parentheses. If so, add relevant number of closing parentheses
-            for i in range(num_open):
-                cur_soln += ')'
-            soln.append(cur_soln)
-            return soln
+        res = []
+        # Base case
+        if num_open == n:
+            if num_closed < n:
+                # Add closing parenthesis until solution is valid
+                for i in range(num_closed, n):
+                    cur_soln.append(')')
+                res.append(''.join(cur_soln))
+                # Remove open parentheses to avoid affecting other recursion branches
+                for i in range(num_closed, n):
+                    cur_soln.pop()
+            else:
+                res.append(''.join(cur_soln))
+            return res
         
-        # Add an open parenthesis to the current solution
-        soln += self._helper(cur_soln + '(', n-1, num_open+1)
-        # Add a closed parenthesis to the current solution
-        soln += self._helper(cur_soln + ')(', n-1, num_open)
+        # Add open parenthesis
+        cur_soln.append('(')
+        res += self._helper(n, cur_soln, num_open+1, num_closed)
+        cur_soln.pop()
 
-        return soln
+        # Add closed parenthesis only if valid
+        if num_closed < num_open:
+            cur_soln.append(')')
+            res += self._helper(n, cur_soln, num_open, num_closed+1)
+            # This line is needed so that when the function returns, the ')' added in this call does not affect recursive calls further up
+            # the call stack
+            cur_soln.pop()
         
+        return res
+
