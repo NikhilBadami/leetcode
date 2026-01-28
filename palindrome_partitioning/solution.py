5a38a2fc49f3004bc4c7deb0eb2caca6f6aacccf
NikhilBadami <nikhilbadami@Nikhils-MacBook-Pro.local>
2026-01-27 21:36:10 -0500

    Optimal solution to palindrome partitioning

--- palindrome_partitioning/solution.py
+++ palindrome_partitioning/solution.py
@@ -1,43 +1,51 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
         """
-        I'm given a string as input and I need to partition it such that every partition of the string is a palindrome. Note that if the input
-        itself is a palindrome, that counts as a valid partition.
+        Hint: You are not building palindromic substrings you are partitioning the string. If you find a valid palindromic sub-string, how does
+        this affect the recursion? Where should you start searching in the recursion?
 
-        What if instead of splitting the string, I expand the window on recursive calls? On the top level, I expand the window to be of size
-        1 character. If this window is a valid palindrome, I add the partition and recurse. On the recursion, I expand the window one more
-        character. If this string is not a palindrome, I return and move the left pointer up one therefore shrinking the window.
+        I am given a string and I need to return all possible splits of the string such that each split sub-string is a valid palindrome. The
+        most straightfoward way to do this is to check all possible splits of the original string, and then all possible splits of every sub-
+        string which guarantees looking at all possible sub-strings. If a valid palindrome is found, I continue searching by treating everything
+        that comes after the valid palindrome as the subsequent string to search. Note that I am not building palindromes I am partitioning
+        the string into palindromes (if possible). This means if I find a valid palindrome, I don't need to continue building on it I can just
+        add it to the running solution.
+
+        time: O(n*2^n) --> At each character I choose to either split or not split at that character. Additionally, I need to check if each
+                           sub-string is a palindrome which takes n time in the worst case
+        memory: O(n) --> I am storing each sub-string I find and the recursion stack is at most n
         """
-        if len(s) == 1:
-            return [[s[0]]]
-        res = []
-        self._helper(s, 0, 0, [], res)
-        return res
+        global_res = []
+        self._helper(s, 0, [], global_res)
+        return global_res
     
-    def _helper(self, s, l, r, res, global_res):
+    def _helper(self, s, l, res, global_res):
         """
-        Helper function takes string, current window pointers and running solution and global solution.
+        Helper function that partitions the given string. Any valid palindromes found are store in res. If the current string
+        is empty, res is added to global_res, which is updated in place
         """
-        # Base cases
-        if r == len(s):
+        if l >= len(s):
             global_res.append(res.copy())
         
-        # Expand the window by 1 and check if the window is a palindrome
-        while r < len(s):
-            r += 1
-            if not self._is_palindrome(s[l:r]):
-                l += 1
-            res.append(s[l:r])
-            self._helper(s, l, r, res, global_res)
+        # Partition string
+        for i in range(l+1, len(s)+1):
+            sub_str = s[l:i]
+            if not self._is_palindrome(sub_str):
+                continue
+            res.append(sub_str)
+            self._helper(s, i, res, global_res)
+            res.pop()
         
-    
     def _is_palindrome(self, s):
         """
-        Checks if a string is a palindrome. Returns true if it is, false otherwise
+        Helper function checks if the given string is a palindrome
         """
         l, r = 0, len(s) - 1
         while l <= r:
             if s[l] != s[r]:
                 return False
+            l += 1
+            r -= 1
         return True
+        
         
