33346c79089e1609a209e97d337718202a33ac15
NikhilBadami <nikhilbadami@Nikhils-MacBook-Pro.local>
2026-02-05 21:00:43 -0500

    Maximum product subarray optimal time solution

--- maximum_product_subarray/solution.py
+++ maximum_product_subarray/solution.py
@@ -1,35 +1,30 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         """
-        I'm given an integer array and I need to find the subarray that has be maximum product. A subarray is a continuous subset of elements
-        within the larger array.
+        I am given an array of numbers and need to find the product of the subarray that has the largest product.
 
-        How to approach this problem? What would be the solution to an array with just one number? It would be just that number. What about
-        if the array contains two positive numbers? The solution would be the product of those two numbers. What about if the array contained n
-        positive numbers? The solution would be the product of all of those numbers. What if there are negatives or 0's in the array? An array 
-        with two elements where one is positive has answer of the positive element, same if the array has a zero and a positive element. If the 
-        array has a zero and a negative element, the answer is 0.
-
-        What if the recurrence is the question, "what is the largest product of the array up to and including the current element?". Example:
-        [-3,2]. The first product would be -3, followed by -6. Since multiplying the second element by the first produces a more negative
-        number, the answer is still the first product, namely -3. However, I cannot use this answer going forward since adding future elements
-        to this running product would break the requirement that they be part of the same sub-array, i.e., the elements would not be
-        contiguous.
-
-        What if I modify the recurrence to be, what is the largest product I can form either adding the current element to the previous
-        product, or starting a new product with the current element? I can use a single variable to track the largest running product
-        and a global variable to track the largest overall product.
+        I can accomplish this by iterating through the array and tracking the various prods of subarrays up to and including the current element.
+        Specifically, I need to track the minimum and maximum sums up to and including that array. This is because of the presence of negative
+        numbers which can flip the sign of very negative products and make them very large positive numbers. Additionally, I need to handle
+        encountering 0. If this happens, I simply reset my tracking variables to 1.
 
         time: O(n)
         memory: O(1)
         """
-        best_prod = nums[0]
-        cur_prod = nums[0]
-        for i in range(1, len(nums)):
-            if nums[i] * cur_prod < nums[i]:
-                cur_prod = nums[i]
+        max_sum = nums[0]
+        cur_max = 1
+        cur_min = 1
+        for i in range(len(nums)):
+            cur_max *= nums[i]
+            cur_min *= nums[i]
+            if nums[i] == 0:
+                cur_max = 1
+                cur_min = 1
+                max_sum = max(0, max_sum)
             else:
-                cur_prod *= nums[i]
-            best_prod = max(best_prod, cur_prod)
-        return best_prod
-
+                tmp = cur_max
+                cur_max = max(cur_max, cur_min, nums[i])
+                cur_min = min(tmp, cur_min, nums[i])
+                max_sum = max(cur_max, max_sum)
+        return max_sum
+        
