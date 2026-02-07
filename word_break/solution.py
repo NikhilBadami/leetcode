aa3dab4f65398a915feac1c09e2daf52bdc24933
NikhilBadami <nikhilbadami@Nikhils-MacBook-Pro.local>
2026-02-06 19:56:34 -0500

    Word break optimal solution

--- word_break/solution.py
+++ word_break/solution.py
@@ -13,33 +13,23 @@ class Solution:
         If I greedily match words, I will match the occurrence of "an" to "an" and end up with "ddog" which does not match any words. This
         algorithm would return False, which is not correct since this problem has a solution.
 
-        A brute force solution could be to try every possible valid paritioning until one works. This would work by iterating over the string
-        and once a valid parition is found, recursing into the problem again, this time with the un-partitioned string as input. If this
-        partitioning fails, return to the original problem and try extending this. If another valid partitioning is found, recurse again.
-        Continue doing this until either a valid partitioning is found or the end of the string is reached.
+        Another way to approach this problem would be to iterate backwards and try and see if I can create substrings based on the words in
+        the dictionary. For example, starting at the end, for every character I test to see if 1: the length of that character to the end is
+        in bounds and 2: if the index after that addition can be segmented. If it can, then this index can be segmented as well. The base case
+        is 1 index after the end of the string (so s[i:] == True)
 
-        time: O(n^2)
-        memory: O(n) --> Will process input wordDict into a set
+        time: O(ndw) --> n is the size of the string, d is the number of words in the dictionary, w is the length of the longest word in the 
+                         dictionary
+        memory: O(n)
         """
-        # Process wordDict into set for O(1) lookup
-        dictionary = set()
-        for w in wordDict:
-            dictionary.add(w)
-        return self._helper(s, dictionary)
-        
-    def _helper(self, s, dictionary):
-        """
-        Takes a string as input and tries to partition it. If a valid parittioning is found, it recurses again using the unpartitioned string
-        as input. If the entire string can be partitioned, returns True, otherwise False
-        """
-        if s in dictionary:
-            return True
-        
-        for i in range(len(s)):
-            if s[0:i] in dictionary:
-                # Found a valid partitioning
-                can_partition = self._helper(s[i:], dictionary)
-                if can_partition:
-                    return True
-        return False
-        
+        dp = [False] * (len(s) + 1)
+        dp[len(s)] = True
+
+        for i in range(len(s), -1, -1):
+            for w in wordDict:
+                if s[i:i+len(w)] == w and (i + len(w) <= len(s)):
+                    dp[i] = dp[i+len(w)]
+                    if dp[i]:
+                        break
+        return dp[0]
+
