46dd256c3f7b25685986e15125074b2f77ddfb2e
NikhilBadami <nikhilbadami@Nikhils-MacBook-Pro.local>
2026-02-20 10:00:34 -0500

    Encode and Decode string optimal solution

--- encode_and_decode_string/solution.py
+++ encode_and_decode_string/solution.py
@@ -1,30 +1,59 @@
 class Solution:
+    """
+    I need to implement two methods: encode and decode. Encode takes a list of strings and
+    encodes them into a single string. Decode then takes that string and splits it back
+    into the original set of strings.
 
-    EMPTY_ARRAY_CODE = "%^&empty%^&"
-    JOIN_STR = "%G(X@Q)"
+    The first thing that comes to mind is to use some kind of special string sequence. This
+    is not robust, however, since the possible input includes all ASCII characters. How
+    can I make the encoding unique to the input?
+
+    One way to make it unique to the input is to encode the length of each string followed
+    by a special separator. For example, given the input ['apple', 'orange'], I could
+    encode it as 5#apple6#orange. This way when I encounter a number followed by a #
+    character, I know that the next n digits correspond to a string.
+    """
+    def __init__(self):
+        self.separator = "#"
 
     def encode(self, strs: List[str]) -> str:
         """
-        Use a special character or set of character to identify breaks
-        between words. In this case, I will use %^& to identify breaks.
-        One edge case here is an empty array. An array with a single
-        value will split back into that single string, but an empty
-        array will reduce to a single empty string, which then decodes back
-        to an array of a single empty string. In this case, we can use
-        a special character to indicate an empty array, perhaps something
-        like "%^&empty%^&"
+        Iterate over the list input and join each string into a single string.
 
-        time: O(n) --> n is the number of strings
-        memory: O(m) --> m is the size of all strings
+        time: O(n)
+        memory: O(n)
         """
-        return self.JOIN_STR.join(strs) if len(strs) != 0 else self.EMPTY_ARRAY_CODE
+        res = []
+        for s in strs:
+            res.append(str(len(s)))
+            res.append(self.separator)
+            res.append(s)
+        return ''.join(res)
+
     def decode(self, s: str) -> List[str]:
         """
-        Now that I have a known special character, simply split the given
-        string based on this character
+        Iterate through the string. Whenever a number followed by the separator is found,
+        check the next n characters after the separator as the next word int he sequence.
 
-        time: O(n) --> n is the number of strings
-        memory: O(m) --> m is the size of all strings
+        time: O(n)
+        memory: O(n)
         """
-        return s.split(self.JOIN_STR) if s != self.EMPTY_ARRAY_CODE else []
+        res = []
+        nums = "0123456789"
+        i = 0
+        while i < len(s):
+            if s[i] in nums:
+                start = i
+                while s[i] in nums:
+                    i += 1
+                if s[i] == self.separator:
+                    n = int(s[start:i])
+                    res.append(s[i+1:i+1+n])
+                    i += (1 + n)
+                else:
+                    i += 1
+            else:
+                i += 1
+        return res
+
 
